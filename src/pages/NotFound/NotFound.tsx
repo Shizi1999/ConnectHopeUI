f3cfd23aaaa25c45091c0b892f995816eef4589b
Shizi1999 <daungocduc1999@gmail.com>
2023-08-09 09:37:46 +0700

    config

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,5 +1,11 @@
+import { Navigate } from 'react-router-dom';
+import path from '~/constant/path';
 function NotFound() {
-  return <div>Not found</div>;
+  return (
+    <div>
+      <Navigate to={path.home} />
+    </div>
+  );
 }
 
 export default NotFound;
